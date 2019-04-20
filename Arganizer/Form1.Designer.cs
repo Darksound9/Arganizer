ff89a3ec78b9873b0ce0f2b523b8e07f96e8989c
Dagan Danevic <dagan.danevic@superion.com>
2019-04-20 10:36:33 -0400

    Initial commit

---
+++ Arganizer/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Arganizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.CreateExcelFileButton = new System.Windows.Forms.Button();
+            this.DriveLabel = new System.Windows.Forms.Label();
+            this.chooseADriveFolderButton = new System.Windows.Forms.Button();
+            this.ScanReadyLabel = new System.Windows.Forms.Label();
+            this.InitiateScanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.Location = new System.Drawing.Point(12, 9);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(316, 13);
+            this.HeaderLabel.TabIndex = 0;
+            this.HeaderLabel.Text = "Welcome! Please choose a location and name for your Excel File:";
+            // 
+            // CreateExcelFileButton
+            // 
+            this.CreateExcelFileButton.Location = new System.Drawing.Point(97, 36);
+            this.CreateExcelFileButton.Name = "CreateExcelFileButton";
+            this.CreateExcelFileButton.Size = new System.Drawing.Size(113, 29);
+            this.CreateExcelFileButton.TabIndex = 4;
+            this.CreateExcelFileButton.Text = "Create Excel File";
+            this.CreateExcelFileButton.UseVisualStyleBackColor = true;
+            this.CreateExcelFileButton.Click += new System.EventHandler(this.BrowseLocationButton_Click);
+            // 
+            // DriveLabel
+            // 
+            this.DriveLabel.AutoSize = true;
+            this.DriveLabel.Location = new System.Drawing.Point(12, 83);
+            this.DriveLabel.Name = "DriveLabel";
+            this.DriveLabel.Size = new System.Drawing.Size(318, 13);
+            this.DriveLabel.TabIndex = 5;
+            this.DriveLabel.Text = "Excel file has been created. Please choose a drive/folder to scan:";
+            this.DriveLabel.Visible = false;
+            // 
+            // chooseADriveFolderButton
+            // 
+            this.chooseADriveFolderButton.Location = new System.Drawing.Point(80, 121);
+            this.chooseADriveFolderButton.Name = "chooseADriveFolderButton";
+            this.chooseADriveFolderButton.Size = new System.Drawing.Size(146, 29);
+            this.chooseADriveFolderButton.TabIndex = 6;
+            this.chooseADriveFolderButton.Text = "Choose a Drive/Folder";
+            this.chooseADriveFolderButton.UseVisualStyleBackColor = true;
+            this.chooseADriveFolderButton.Visible = false;
+            this.chooseADriveFolderButton.Click += new System.EventHandler(this.chooseADriveFolderButton_Click);
+            // 
+            // ScanReadyLabel
+            // 
+            this.ScanReadyLabel.AutoSize = true;
+            this.ScanReadyLabel.Location = new System.Drawing.Point(12, 174);
+            this.ScanReadyLabel.Name = "ScanReadyLabel";
+            this.ScanReadyLabel.Size = new System.Drawing.Size(79, 13);
+            this.ScanReadyLabel.TabIndex = 7;
+            this.ScanReadyLabel.Text = "Ready to scan!";
+            this.ScanReadyLabel.Visible = false;
+            // 
+            // InitiateScanButton
+            // 
+            this.InitiateScanButton.Location = new System.Drawing.Point(97, 207);
+            this.InitiateScanButton.Name = "InitiateScanButton";
+            this.InitiateScanButton.Size = new System.Drawing.Size(113, 32);
+            this.InitiateScanButton.TabIndex = 8;
+            this.InitiateScanButton.Text = "Begin Scan";
+            this.InitiateScanButton.UseVisualStyleBackColor = true;
+            this.InitiateScanButton.Visible = false;
+            this.InitiateScanButton.Click += new System.EventHandler(this.InitiateScanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(338, 78);
+            this.Controls.Add(this.InitiateScanButton);
+            this.Controls.Add(this.ScanReadyLabel);
+            this.Controls.Add(this.chooseADriveFolderButton);
+            this.Controls.Add(this.DriveLabel);
+            this.Controls.Add(this.CreateExcelFileButton);
+            this.Controls.Add(this.HeaderLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arganizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.Button CreateExcelFileButton;
+        private System.Windows.Forms.Label DriveLabel;
+        private System.Windows.Forms.Button chooseADriveFolderButton;
+        private System.Windows.Forms.Label ScanReadyLabel;
+        private System.Windows.Forms.Button InitiateScanButton;
+    }
+}
+
---
+++ Arganizer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Arganizer
+{
+    public partial class Form1 : Form
+    {
+        public string excelFileName = "";
+        public string scanPath = "";
+        public int currentRow = 2;
+
+        ExcelPackage package = new ExcelPackage();
+        ExcelWorksheet worksheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void BrowseLocationButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                CheckPathExists = true,
+                AddExtension = true,
+                OverwritePrompt = true,
+                ValidateNames = true,
+                FileName = "",
+                Filter = "Excel Workbook|*.xlsx|Excel 97-2003 Workbook|*.xls",
+                Title = "Save Excel File"
+            };
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK && saveFileDialog1.FileName != "")
+            {
+                excelFileName = saveFileDialog1.FileName;
+                this.HeaderLabel.Text = excelFileName;
+                // Saves the Image via a FileStream created by the OpenFile method.  
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // NOTE that the FilterIndex property is one-based.  
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                    case 2:
+                        worksheet = package.Workbook.Worksheets.Add("Base Worksheet");
+                        worksheet.Cells[1, 1].Value = "Directory";
+                        worksheet.Cells[1, 2].Value = "Prefix";
+                        worksheet.Cells[1, 3].Value = "Name";
+                        worksheet.Cells[1, 4].Value = "Extension";
+                        package.SaveAs(fs);
+                        fs.Close();
+                        break;
+                }
+
+                fs.Close();
+            }
+
+            System.IO.FileStream fss = new System.IO.FileStream(excelFileName, System.IO.FileMode.Open);
+            fss.Close();
+            this.DriveLabel.Visible = true;
+            this.chooseADriveFolderButton.Visible = true;
+            this.Height = 208;
+        }
+
+        private void chooseADriveFolderButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog
+            {
+                Description = "Choose a Drive/Folder"
+            })
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    scanPath = fbd.SelectedPath;
+                    this.DriveLabel.Text = scanPath;
+
+
+                    this.ScanReadyLabel.Visible = true;
+                    this.InitiateScanButton.Visible = true;
+                    this.Height = 300;
+                }
+            }
+        }
+
+        private void InitiateScanButton_Click(object sender, EventArgs e)
+        {
+            InitiateFolderScan(scanPath);
+        }
+
+        private void InitiateFolderScan(string currentDirectory)
+        {
+            foreach (string filePath in Directory.GetFiles(currentDirectory))
+            {
+                //string[] splitBySlash = filePath.Split("//");
+            }
+            foreach(string dir in Directory.GetDirectories(currentDirectory))
+            {
+                InitiateFolderScan(dir);
+            }
+        }
+    }
+}
